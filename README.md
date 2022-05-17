eeb73a77a9c312dea4d6665511b58053817f1231
agatumm420 <agnieszkatumm@gmail.com>
2022-05-17 10:18:21 +0200

    with read-me

---
+++ README.md
@@ -0,0 +1,4 @@
+## This node backend is created for user_and_feed project
+
+Works with existing Firebase infrastructure and connects it to maraia.db database on a server.
+For the changes to be read you have to start it with npm start
--- controllers/db.js
+++ controllers/db.js
@@ -1,7 +1,7 @@
 var admin = require("firebase-admin");
-const { getDatabase } = require('firebase-admin/database');
+const { getDatabase } = require("firebase-admin/database");
 var serviceAccount = require("./serviceAccountKey.json");
-const mariadb = require('mariadb');
+const mariadb = require("mariadb");
 
 // admin.initializeApp({
 //     credential: admin.credential.cert(serviceAccount),
@@ -19,53 +19,80 @@ const mariadb = require('mariadb');
 //    ref.on('child_added', (snap) => {
 //       count++;
 //       console.log('added:', snap.key);
-  
-  
+
 //     });
 
-exports.db=(req, res)=>{
-    const pool = mariadb.createPool({ socketPath:'/run/mysqld/mysqld.sock', user:'aga', password:'password', database:'app'});
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        // The database URL depends on the location of the database
-        databaseURL: "https://projekt-up-default-rtdb.firebaseio.com"
-    });
-    const db = getDatabase();
-    var ref = db.ref("users");
-    ref.once("value", function(snapshot) {
-      console.log(snapshot.val());
-    });
-   // const usersRef = ref.child('users');
-   let count=0;
-   ref.on('child_added', (snap) => {
-      count++;
-      console.log('added:', snap.key);
-      pool.getConnection()
-      .then(conn => {
+exports.db = (req, res) => {
+  const pool = mariadb.createPool({
+    socketPath: "/run/mysqld/mysqld.sock",
+    user: "aga",
+    password: "password",
+    database: "app",
+  });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    // The database URL depends on the location of the database
+    databaseURL: "https://projekt-up-default-rtdb.firebaseio.com",
+  });
+  const db = getDatabase();
+  var ref = db.ref("users");
+  ref.once("value", function (snapshot) {
+    console.log(snapshot.val());
+  });
+  // const usersRef = ref.child('users');
+  let count = 0;
+  ref.on("child_added", (snap) => {
+    count++;
+    console.log("added:", snap.key);
+    pool
+      .getConnection()
+      .then((conn) => {
         console.log("connected ! connection id is " + conn.threadId);
-        conn.query("INSERT INTO users value (?, ?, ?, ?, ?)",[snap.val().user_id, snap.val().username, snap.val().password, snap.val().name, snap.val().email])
-        .then(res=> console.log(res));
+        conn
+          .query("INSERT INTO users value (?, ?, ?, ?, ?)", [
+            snap.val().user_id,
+            snap.val().username,
+            snap.val().password,
+            snap.val().name,
+            snap.val().email,
+          ])
+          .then((res) => console.log(res));
         //conn.query(`INSERT INTO users(email, name, password, user_id, username) VALUES (${snap.val().email}, ${snap.val().name}, ${snap.val().password},${snap.val().user_id}, ${snap.val().username})`)
         conn.release(); //release to pool
       })
-      .catch(err=> console.log(err));
-  
-    });
-    
-    var feed_ref=db.ref("feed");
-    feed_ref.on('child_added', (snap) => {
-      count++;
-      console.log('added:', snap.key);
-      pool.getConnection()
-      .then(conn=>{
+      .catch((err) => console.log(err));
+  });
+
+  var feed_ref = db.ref("feed");
+  feed_ref.on("child_added", (snap) => {
+    count++;
+    console.log("added:", snap.key);
+    pool
+      .getConnection()
+      .then((conn) => {
         console.log("connected ! connection id is " + conn.threadId);
-        conn.query("INSERT INTO feed value(?,?,?)",[snap.val().feed_id, snap.val().feed_txt, snap.val().user_id])
-        .then(res=>console.log(res));
+        conn
+          .query("INSERT INTO feed value(?,?,?)", [
+            snap.val().feed_id,
+            snap.val().feed_txt,
+            snap.val().user_id,
+          ])
+          .then((res) => console.log(res));
         conn.release();
       })
-      .catch(err=>console.log(err));
+      .catch((err) => console.log(err));
+  });
+  res.sendStatus(count);
+  feed_ref.on("child_removed", (snapshot) => {
+    pool.getConnection().then((conn) => {
+      console.log("connected ! connection id is " + conn.threadId);
+      conn
+        .query("DELETE FROM feed WHERE user_id=value(?)", [
+          snapshot.val().user_id,
+        ])
+        .then((res) => console.log(res));
+      //conn.query(`INSERT INTO users(email, name, password, user_id, username) VALUES (${snap.val().email}, ${snap.val().name}, ${snap.val().password},${snap.val().user_id}, ${snap.val().username})`)
+      conn.release(); //release to pool
     });
-    res.sendStatus(count);
-}
-
-  
+  });
+};
